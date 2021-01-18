eda5c0b7bd2e7f1df8bf4efc45e4c561679f77ed
Hakkyu Kim <hakkyu.kim@samsung.com>
2021-01-19 03:43:25 +0900

    Update build and test scripts

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @ECHO off
-cmake -S . -B out -G "MinGW Makefiles" -DCMAKE_CXX_COMPILER=g++
-mingw32-make -C out
+cmake -S . -B out
+msbuild out\src\driver.vcxproj
--- run.bat
+++ run.bat
@@ -1,2 +1,13 @@
-out\src\Debug\driver.exe tests\project\07\MemoryAccess\BasicTest\BasicTest.vm tests\project\07\MemoryAccess\BasicTest\BasicTest.asm
+@ECHO off
+out\src\Debug\driver.exe tests\project\07\StackArithmetic\SimpleAdd\SimpleAdd.vm 
+out\src\Debug\driver.exe tests\project\07\StackArithmetic\StackTest\StackTest.vm 
+out\src\Debug\driver.exe tests\project\07\MemoryAccess\BasicTest\BasicTest.vm 
+out\src\Debug\driver.exe tests\project\07\MemoryAccess\StaticTest\StaticTest.vm 
+out\src\Debug\driver.exe tests\project\07\MemoryAccess\PointerTest\PointerTest.vm 
 
+out\src\Debug\driver.exe tests\project\08\ProgramFlow\BasicLoop\BasicLoop.vm 
+out\src\Debug\driver.exe tests\project\08\ProgramFlow\FibonacciSeries\FibonacciSeries.vm 
+
+out\src\Debug\driver.exe tests\project\08\FunctionCalls\SimpleFunction\SimpleFunction.vm 
+out\src\Debug\driver.exe tests\project\08\FunctionCalls\FibonacciElement -bootstrap 261
+out\src\Debug\driver.exe tests\project\08\FunctionCalls\StaticsTest 
